750fc81b056a339b17eccb7307bd9dc2bb3539f2
Hashmat1985 <hazimi2018@gmail.com>
2023-05-17 23:03:02 -0700

    Initial commit from Instructor

---
+++ src/tek/instructor/MohammadClass.java
@@ -0,0 +1,11 @@
+package tek.instructor;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class MohammadClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This Is Mohammad. Tekschool Instructor");
+	}
+}
