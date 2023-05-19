9e492dd91ca3a3be1a944aabc70ad036dbd4849f
Hashmat1985 <hazimi2018@gmail.com>
2023-05-18 22:46:10 -0700

    new branch change

---
+++ src/tek/hashmatullah/HashmatullahClass.java
@@ -0,0 +1,10 @@
+package tek.hashmatullah;
+
+public class HashmatullahClass {
+	public static void main(String[] args) {
+		
+		System.out.println("This is Hashmatullah dragons student");
+		
+	}
+
+}
